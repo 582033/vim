9250f787e03c3a9b37fe3ba6c73a1e6175d1831d
yjiang <yan.jiang@marsmarch.cn>
2021-11-22 11:40:30 +0800

    fix

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ yjiang_symlinks() {
 
 setup_packer() {
     if [ ! -e "$app_dir/plugin" ]; then
-	git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+	git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/opt/packer.nvim
     fi
     nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 }
