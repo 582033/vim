672e00b9788591d1dfcd6b1988fe43abda9e90a8
yjiang <yan.jiang@marsmarch.cn>
2021-11-22 11:34:38 +0800

    fix

--- lua/lang/go.lua
+++ lua/lang/go.lua
@@ -10,7 +10,20 @@ vim.g.go_highlight_fields = 1
 vim.g.go_highlight_functions = 1
 vim.g.go_highlight_function_calls = 1
 vim.g.go_highlight_extra_types = 1
+
+
+vim.g.go_highlight_array_whitespace_error = 1
+vim.g.go_highlight_chan_whitespace_error = 1
+vim.ggo_highlight_space_tab_error = 1
+vim.g.go_highlight_trailing_whitespace_error = 1
+vim.g.go_highlight_operators = 1
+vim.g.go_highlight_function_parameters = 1
+vim.ggo_highlight_build_constraints = 1
+vim.g.go_highlight_string_spellcheck = 1
+vim.g.go_highlight_variable_declarations = 1
 vim.g.go_highlight_generate_tags = 1
+vim.g.go_highlight_variable_assignments = 1
+vim.g.go_highlight_diagnostic_warnings = 1
 
 --回车映射，防止补全提示重复
 vim.cmd('au FileType go nmap <leader>r <Plug>(go-run)')
