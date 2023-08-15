483ed15bc8d45a981dc9173114b9ff99e02be49d
Yan Jiang <j582033@163.com>
2023-08-15 17:18:00 +0800

    删除废弃的systemd支持及替换copilot为codeium

--- lua/plugin/packer.lua
+++ lua/plugin/packer.lua
@@ -33,13 +33,24 @@ packer.startup({
 		-- 包管理工具Packer
 		use 'wbthomason/packer.nvim'
 		-- Copilot
-		use 'github/copilot.vim'
+		--use 'github/copilot.vim'
+		-- codeium
+		use {
+			'Exafunction/codeium.vim',
+			config = function ()
+				-- vim.keymap.set('i', '<Tab>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+				-- Change '<C-g>' here to any keycode you like.
+				-- vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+				-- vim.keymap.set('i', '<c-.>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+				-- vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+				-- vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+			end
+
+		}
 		--启动界面快速打开最近的文件
 		use 'mhinz/vim-startify'
 		-- nginx 配置文件高亮
 		use 'chr4/nginx.vim'
-		-- systemd 系统文件高亮
-		use 'Matt-Deacalion/vim-systemd-syntax'
 		-- php
 		--use 'spf13/PIV', { 'for': 'php' }
 		--代码片段工具
