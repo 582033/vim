f974039f2ea23dd5b48b2bb0bdb6cb88cb0c9851
Yan Jiang <j582033@163.com>
2024-03-19 17:02:00 +0800

    修复更新x-ray后的golines配置

--- lua/plugin/go.lua
+++ lua/plugin/go.lua
@@ -33,7 +33,7 @@ require('go').setup({
 	goimport='gopls', -- goimport command, can be gopls[default] or goimport
 	fillstruct = 'gopls', -- default, can also use fillstruct
 	gofmt = 'gofumpt', --gofmt cmd,
-	max_line_len = 128, -- max line length in golines format, Target maximum line length for golines
+	--max_line_len = 128, -- max line length in golines format, Target maximum line length for golines
 	tag_transform = false, -- can be transform option("snakecase", "camelcase", etc) check gomodifytags for details and more options
 	tag_options = 'json=omitempty', -- sets options sent to gomodifytags, i.e., json=omitempty
 	gotests_template = "", -- sets gotests -template parameter (check gotests for details)
